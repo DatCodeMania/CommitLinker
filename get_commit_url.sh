a1bf786cc45ed42c6ea9a8fd9746db9dd9fbe101
DatCodeMania <147077877+DatCodeMania@users.noreply.github.com>
2024-07-03 16:18:39 +0700

    Update get_commit_url.sh

--- get_commit_url.sh
+++ get_commit_url.sh
@@ -4,7 +4,7 @@
 latest_commit=$(git rev-parse HEAD)
 
 # Your GitHub repository URL
-repo_url="https://github.com/DatCodeMania/GymTracker"
+repo_url="https://github.com/USERNAME/REPO"
 
 # Construct the commit URL
 commit_url="${repo_url}/commit/${latest_commit}"
